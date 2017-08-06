fbff6c4a66501cc592f10aff1c4aa51504b332d7
Caylin James <caylin@lougeek.com>
2017-08-06 14:28:39 -0400

    Resetting the Form (complete)

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output } from "@angular/core";
-
+import { Component, EventEmitter, Output, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 @Component({
     selector: "app-entry-comment-form",
     templateUrl: "entry-comment-form.component.html",
@@ -8,8 +8,10 @@ export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
-    onSubmit() {
+    @ViewChild("commentForm") commentForm: NgForm;
+    onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment };
         this.onCommentAdded.emit(comment);
+        this.commentForm.resetForm();
     }
 }
